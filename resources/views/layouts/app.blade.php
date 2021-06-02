c286791548239e81f15b0384525c3fa8abf3521a
yogiIDL <yogi_tarigan@idlcargo.co.id>
2021-06-02 14:51:40 +0700

    Fix Menu side bar di Dashboard (unfinished)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -140,16 +140,25 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <span>Task</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
+                        <h6 class="collapse-header">List Task</h6>
+                        <a class="collapse-item" href="buttons.html">Dispatch</a>
+                        <a class="collapse-item" href="buttons.html">Traffic</a>
+                        <a class="collapse-item" href="buttons.html">Ground Handling</a>
                     </div>
                 </div>
             </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                
+            </div>
 
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
@@ -161,7 +170,7 @@
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
+                        <h6 class="collapse-header">Level Management</h6>
                         <a class="collapse-item" href="utilities-color.html">Colors</a>
                         <a class="collapse-item" href="utilities-border.html">Borders</a>
                         <a class="collapse-item" href="utilities-animation.html">Animations</a>
@@ -175,26 +184,27 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Addons
+                Admin Page
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
+            {{-- User Management Sidebar --}}
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                     aria-expanded="true" aria-controls="collapsePages">
                     <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
+                    <span>User Management</span>
                 </a>
                 <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Login Screens:</h6>
-                        <a class="collapse-item" href="login.html">Login</a>
-                        <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <div class="collapse-divider"></div>
+                        <h6 class="collapse-header">User Utilities</h6>
+                        <a class="collapse-item" href="login.html">Register New User</a>
+                        <a class="collapse-item" href="login.html">List All User</a>
+                        <a class="collapse-item" href="register.html">Manage User</a>
+                        {{-- <div class="collapse-divider"></div>
                         <h6 class="collapse-header">Other Pages:</h6>
                         <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a>
+                        <a class="collapse-item" href="blank.html">Blank Page</a> --}}
                     </div>
                 </div>
             </li>
