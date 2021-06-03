b15523baeeacdc73310259f6c78e89718b118d96
yogiIDL <yogi_tarigan@idlcargo.co.id>
2021-06-03 14:52:55 +0700

    Fix menu sidebar di Dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,7 +139,7 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
+                    <i class="fas fa-fw fa-tasks"></i>
                     <span>Task</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
@@ -157,24 +157,23 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                
+               Level Page 
             </div>
 
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+                    <i class="fas fa-fw fa-key"></i>
+                    <span>Access</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Level Management</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
+                        <a class="collapse-item" href="utilities-color.html">New Access</a>
+                        <a class="collapse-item" href="utilities-border.html">Manage</a>
                         <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
                     </div>
                 </div>
             </li>
@@ -192,13 +191,13 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                     aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
+                    <i class="fas fa-fw fa-users-cog"></i>
                     <span>User Management</span>
                 </a>
                 <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">User Utilities</h6>
-                        <a class="collapse-item" href="login.html">Register New User</a>
+                        <a class="collapse-item" href="/register">Register New User</a>
                         <a class="collapse-item" href="login.html">List All User</a>
                         <a class="collapse-item" href="register.html">Manage User</a>
                         {{-- <div class="collapse-divider"></div>
@@ -207,20 +206,6 @@
                         <a class="collapse-item" href="blank.html">Blank Page</a> --}}
                     </div>
                 </div>
-            </li>
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-            </li>
-
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
             </li>
 
             <!-- Divider -->
@@ -523,7 +508,7 @@
 
     <!-- Bootstrap core JavaScript-->
     {{-- <script src="vendor/jquery/jquery.min.js"></script> --}}
-    <script src="{{ url('vendor/jquery/jquery.min.js') }}"></script>j
+    <script src="{{ url('vendor/jquery/jquery.min.js') }}"></script>
     {{-- <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> --}}
     <script src="{{ url('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
