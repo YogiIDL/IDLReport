09dbd0264e7f215d34905caace3177251e3a3016
yogiIDL <yogi_tarigan@idlcargo.co.id>
2021-06-08 13:35:33 +0700

    add list user locationlist in user Auth::user()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,27 +38,106 @@ class HomeController extends Controller
         // dump(Auth::user()->attributes);
         // var_dump(Auth::user());
         $user = Auth::user();
+
+        $user->locationlist = array();
+
+        $master = DB::select('select * from master where user_id = ?', [$user->id]);
+
+        $user->master = $master;
+        foreach($master as $i => $item){
+            $user->locationlist = array_unique(array_merge($user->locationlist, [$item->location_id]));
+        }
+
+        dump($user);
+        // die();
+
+        // dump($master);
+        // var_dump(get_object_vars($master[0]));
+        // dump(get_object_vars($master[0]));
+        // $arrmaster = get_object_vars($master[0]);
+        // dump($arrmaster['id']);
+
+        // // $user->locationlist[0] = "2";
+        
+        // foreach($master as $i=>$item){
+        //     // $master[$item] = get_object_vars($master[$item]);
+        //     // dump($master[$item]);
+        //     $item = get_object_vars($item);
+        //     $master[$i] = $item;
+        //     dump($item);
+        //     // $user->locationlist[$i] = $master[$i]["location"];
+        //     // $user->locationlist[$i] = "i";
+        //     // $user->locationlist[$i]['id'] = '1';
+
+        //     // $asd[$i] = $user->master[$i]['location_id'];
+            
+        //     // dump($user->master[0]['id']);
+        // }
+        // $user->master = $master;
+
+        // // dump($asd);
+
+        // // var_dump($master);
+        // // dump($master);
+
+        // dump($user->master[0]['id']);
+
+        // foreach($user->master as $item){
+
+        // }
+
+        // // $asdb = 
+
+        // // $location = array_unique($)
+
+        // // $user->location ="location";
+
         // dump($user);
 
-        $attr = DB::select('select * from master where user_id = ?', [$user->id]);
-        // dump($attr);
+        // dump($user->master[0]);
+
+        // die();
+        // $attr = json_decode($attr);
+
+        // $arr = get_object_vars($attr[0]);
+        // dump($arr);
+        // var_dump($arr['id']);
+
+        // $user->master = $arr;
+        // dump($user);
+       
+        // dump($arr[id]);
+        // var_dump($attr);
+        // die();
+
+        // $user->master=$attr;
+
+        // $user->master = $attr;
+
+        // dump($user);
+
+        // var_dump($user->master);
+
+        // dump($user->master[0]->location);
+        // var_dump($user->master[0]);
+        // dump($user->master[0]);
 
         // $location = array("oke", "abc", "wqe", "wqe", "qwe", "qwe");
         // $location[0] = "askjdnkjasnd";
-        $location = array();
-        foreach($attr as $item => $i){
+        // $location = array();
+        // foreach($attr as $item => $i){
             // dump($item);
             // dump($item->location_id);
             // $location == $item->location_id;
             // var_dump( $item->location_id);
-            dump($i);
+            // dump($i);
             // dump($item);
             // $location[$item] ==$i;
             // dump($location[$item]);
-            $location[$item] == $i;
+            // $location[$item] == $i;
 
-        }
-        dump($location);
+        // }
+        // dump($location);
 
 
         // foreach($attr as $item =>$i){
