98edd9b2c4b434bebd4f66369427f64c15c86fe1
Yogi Adianta <yogiadianta0@gmail.com>
2021-06-09 08:18:33 +0700

    Fix view add area

--- resources/views/Area/addArea.blade.php
+++ resources/views/Area/addArea.blade.php
@@ -1,37 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center">
-        <h1>Add Area</h1>
-    </div>
-
-    <div>
-        <form action="{{url('addArea')}}" method="POST">
-            {{csrf_field()}}
-            <div class="form row">
-                <div class="form-group col-md-4"></div>
-                <div class="form-group col-md-4">
-                    <label for="area">Area</label>
-                    <input type="text" class="form-control" name="area" id="area" placeholder="Area...">
-                </div>
-                <div class="form-group col-md-4"></div>
+<div class="row">
+    <div class="col-lg-3"></div>
+    <div class="col-lg-6">
+        <div class="card shadow mb-12">
+            <div class="card-header py-3">
+                <h5 class="m-0 font-weight-bold text-primary">Add Area</h5>
             </div>
+            <div class="card-body">
+                <form action="{{url('addArea')}}" method="POST">
+                    {{csrf_field()}}
+                    <div class="form row">
+                        <div class="form-group col-md-2"></div>
+                        <div class="form-group col-md-8">
+                            <label for="area">Area</label>
+                            <input type="text" class="form-control" name="area" id="area" placeholder="Area...">
+                        </div>
+                        <div class="form-group col-md-2"></div>
+                    </div>
             
-            <div class="form row">
-                <div class="form-group col-md-2"></div>
-                <div class="form-group col-md-4">
-                    <div class="col-sm-6">
-                        <button class="btn btn-lg btn-primary">Simpan</button>
+                    <div class="form row">
+                        <div class="form-group col-md-2"></div>
+                        <div class="form-group col-md-4">
+                            <div class="col-sm-6">
+                                <button class="btn btn-lg btn-primary">Simpan</button>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <div class="col-sm-6">
+                                <button class="btn btn-lg btn-secondary">Cancel</button>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-2"></div>
                     </div>
-                </div>
-                <div class="form-group col-md-4">
-                    <div class="col-sm-6">
-                        <button class="btn btn-lg btn-secondary">Cancel</button>
-                    </div>
-                </div>
-                <div class="form-group col-md-2"></div>
+                </form>
             </div>
-        </form>
-
+        </div>
     </div>
+    <div class="col-lg-3"></div>
+</div>
 @endsection
