3d53ef22283a2ab517ca72e3a5c099cbd2e2f945
yogiIDL <yogi_tarigan@idlcargo.co.id>
2021-06-11 11:59:12 +0700

    Add user atrribute to Auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,19 +24,43 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        // $user = Auth::user();
+    }
 
-        // Check attribute user master table -> Auth::(User)->attribute;
-        // $user->locationname = array();
-        // $master = DB::select('select * from master 
-        //                     join location on master.location_id = location.id 
-        //                     join area on location.area_id = area.id 
-        //                     join task on master.task_id = task.id 
-        //                     join activity on master.activity_id = activity.id where user_id = ? ', [$user->id] );
-        // $user->master = $master;
-        // foreach($master as $i => $item){
-        //     $user->locationname = array_unique(array_merge($user->locationname, [$item->location_name]));
-        // }
+    public function userAuth(){
+        $user = Auth::user();
+        $user->locationlist = array();
+        $user->tasklist = array();
+        $user->activitylist = array();
+
+        $master = DB::select('select * from master 
+                            join users on master.user_id = users.id
+                            join level on master.level_id = level.id
+                            join location on master.location_id = location.id 
+                            join area on location.area_id = area.id 
+                            join task on master.task_id = task.id 
+                            where user_id = ? ', [$user->id] );
+
+        $activity = DB::select('select * from user_activity 
+                                join users on user_activity.user_id = users.id
+                                join activity on user_activity.activity_id = activity.id
+                                where user_id = ?', [$user->id]);
+        dump($activity);
+
+        foreach($activity as $item){
+            $user->activitylist = array_merge($user->activitylist, [$item->activity_name]);
+        }
+
+        dump($master);
+
+        $user->master = $master;
+        foreach($master as $i => $item){
+            $user->locationlist = array_unique(array_merge($user->locationlist, [$item->location_name]));
+            $user->tasklist = array_unique(array_merge($user->tasklist, [$item->task_name]));
+        }
+
+        dump($user);
+
+        die();
     }
 
     /**
@@ -46,34 +70,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        // dump(Auth::user()->level);
-        // var_dump(Auth::user()->level);
-        // dump(Auth::user()->attributes);
-        // var_dump(Auth::user());
-        $user = Auth::user();
-
-        $user->locationlist = array();
-        $user->locationname = array();
-        $user->tasklist = array();
-        $user->activitylist = array();
-
-
-        $master = DB::select('select * from master 
-                            join location on master.location_id = location.id 
-                            join area on location.area_id = area.id 
-                            join task on master.task_id = task.id 
-                            join activity on master.activity_id = activity.id where user_id = ? ', [$user->id] );
-
-        // dump($master);
-
-        $user->master = $master;
-        foreach($master as $i => $item){
-            $user->locationlist = array_unique(array_merge($user->locationlist, [$item->location_id]));
-            $user->locationname = array_unique(array_merge($user->locationname, [$item->location_name]));
-
-            $user->tasklist = array_unique(array_merge($user->tasklist, [$item->task_id]));
-            $user->activitylist = array_unique(array_merge($user->activitylist, [$item->activity_id]));
-        }
+        $this->userAuth();
 
         return view('home');
     }
