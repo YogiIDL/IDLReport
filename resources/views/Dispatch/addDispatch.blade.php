c61661ff6ac7a98b7d90c9a9db2a6922a8b97dee
yogiIDL <yogi_tarigan@idlcargo.co.id>
2021-06-21 17:10:59 +0700

    Fix View Add Dispatch after Search from API (Unfinished Yet)

--- resources/views/Dispatch/addDispatch.blade.php
+++ resources/views/Dispatch/addDispatch.blade.php
@@ -1,33 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <a href="/addDispatch/{{Auth::user()->locationnow}}" class="btn btn-success btn-sm btn-icon-split">
-        <span class="icon text-white-50">
-            <i class="fas fa-plus"></i>
-        </span>
-        <span class="text">Add Dispatch Report</span>
-    </a>
-</div>
+<div class="row">
+    <div class="col-lg-3"></div>
+    <div class="col-lg-6">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h5 class="m-0 font-weigth-bold text-primary">Add Dispatch</h5>
+            </div>
+            <div class="card-body">
+                {{-- Search Report with API --}}
+                <div class="row">
+                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                        <div class="input-group">
+                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                                aria-label="Search" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="button">
+                                    <i class="fas fa-search fa-sm"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
 
-<div class="card shadow mb-4">
-    <div class="card-header py-3">
-        <h5 class="m-0 font-weigth-bold text-primary">Add Dispatch</h5>
-    </div>
-    <div class="card-body">
-        {{-- Search Report with API --}}
-        <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-            <div class="input-group">
-                <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                    aria-label="Search" aria-describedby="basic-addon2">
-                <div class="input-group-append">
-                    <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                    </button>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="kurir" class="form-control">Nama Kurir</label>
+                        <input type="text">
+                    </div>
+                    <div class="col-lg-6">
+
+                    </div>
                 </div>
             </div>
-        </form>
+        </div>
     </div>
+    <div class="col-lg-3"></div>
 </div>
 
 @endsection
